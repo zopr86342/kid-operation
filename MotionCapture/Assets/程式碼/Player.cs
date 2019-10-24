c4ef37cb93abefd01582008d6c546d22402c2a68
zopr86342 <55218489+zopr86342@users.noreply.github.com>
2019-10-24 10:22:38 +0800

    用程式讓按鍵可以控制動作

--- MotionCapture/Assets/程式碼/Player.cs
+++ MotionCapture/Assets/程式碼/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public Animator robot;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,17 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if(Input.GetKey(KeyCode.R)){
+            robot.SetBool("跑步開關", true);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            robot.SetBool("跑步開關", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            robot.SetTrigger("跳舞觸發");
+        }
+
+    }
 }
